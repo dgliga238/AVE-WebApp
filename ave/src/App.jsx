a6ed4c7786b8272a92b3c02ce41ba82942013d37
dgliga238 <144121694+dgliga238@users.noreply.github.com>
2025-05-10 20:20:47 +0300

    navbar created (#1)
    Co-authored-by: Gliga Daniela <dgliga238@gmail.com>

--- ave/src/App.jsx
+++ ave/src/App.jsx
@@ -2,12 +2,17 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Navbar from './components/Navbar'
+import MobNavBar from './components/MobNavBar'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className='text-5xl underline'>AVE APP</div>
+    <main>
+      <Navbar />
+      <MobNavBar />
+    </main>
   ) 
       
 }
